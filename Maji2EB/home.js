976278e09ddf99403e0ee45c7f5c6dd9ad04566e
tunaCunha <33427531+tunaCunha@users.noreply.github.com>
2022-04-29 01:35:18 -0400

    Add files via upload

---
+++ Maji2EB/home.js
@@ -0,0 +1,39 @@
+function bold() {
+    document.getElementById("textarea1").style.fontWeight = "bold";
+}
+
+function italic() {
+    document.getElementById("textarea1").style.fontStyle = "italic";
+}
+
+function left() {
+    document.getElementById("textarea1").style.textAlign = "left";
+}
+
+function center() {
+    document.getElementById("textarea1").style.textAlign = "center";
+}
+
+function right() {
+    document.getElementById("textarea1").style.textAlign = "right";
+}
+
+function upper() {
+    document.getElementById("textarea1").style.textTransform = "uppercase";
+}
+
+function lower() {
+    document.getElementById("textarea1").style.textTransform = "lowercase";
+}
+
+function cap() {
+    document.getElementById("textarea1").style.textTransform = "capitalize";
+}
+
+/*function clear() {
+    document.getElementById("textarea1").style.fontWeight = "normal";
+    document.getElementById("textarea1").style.textAlign = "left";
+    document.getElementById("textarea1").style.fontStyle = "normal";
+    document.getElementById("textarea1").style.textTransform = "capitalize";
+    document.getElementById("textarea1").value = " ";
+}*/
